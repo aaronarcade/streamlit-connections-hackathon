4bc74a47c5332ccc32cdcc5364ecf73c7fab7dc7
Aaron Brown <aaronarcade@gmail.com>
2023-07-21 20:07:40 -0400

    added duckdb to requirements

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Streamlit App](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)]([https://google.com))
+
 # Streamlit HuggingFace Connection Demo
 
 A little MVP demo of connecting a Streamlit app to HuggingFace data using FilesConnection and HfFileSystem.
@@ -9,5 +11,3 @@ Relevant references:
 - [FilesConnection](https://github.com/streamlit/files-connection)
 - [HfFileSystem Guide](https://huggingface.co/docs/huggingface_hub/main/en/guides/hf_file_system)
 - [Streamlit experimental_connection Reference](https://docs.streamlit.io/library/api-reference/connections)
-
-[![Streamlit App](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)]([https://share.streamlit.io/streamlit/example-app-cv-model/main](https://share.streamlit.io/zakariachowdhury/streamlit-map-dashboard/main))
--- duckdb_connection.py
+++ duckdb_connection.py
@@ -25,3 +25,26 @@ class DuckDBConnection(ExperimentalBaseConnection[duckdb.DuckDBPyConnection]):
             return cursor.df()
 
         return _query(query, **kwargs)
+
+
+class DuckDBConnection(ExperimentalBaseConnection[duckdb.DuckDBPyConnection]):
+    """Basic st.experimental_connection implementation for DuckDB"""
+
+    def _connect(self, **kwargs) -> duckdb.DuckDBPyConnection:
+        if 'database' in kwargs:
+            db = kwargs.pop('database')
+        else:
+            db = self._secrets['database']
+        return duckdb.connect(database=db, **kwargs)
+
+    def cursor(self) -> duckdb.DuckDBPyConnection:
+        return self._instance.cursor()
+
+    def query(self, query: str, ttl: int = 3600, **kwargs) -> pd.DataFrame:
+        @cache_data(ttl=ttl)
+        def _query(query: str, **kwargs) -> pd.DataFrame:
+            cursor = self.cursor()
+            cursor.execute(query, **kwargs)
+            return cursor.df()
+
+        return _query(query, **kwargs)
